4dd568000486731f5c9a99a8b56c14ebb9dcc6e4
VishwanthSetty <vishwanthsetty88@gmail.com>
2019-01-31 14:45:05 +0530

    seeds data set comepelted

--- Assignment-1/Cross-Validation.c
+++ Assignment-1/Cross-Validation.c
@@ -2,7 +2,7 @@
 #include<stdlib.h>
 #include<string.h>
 #include<math.h>
-
+#define r 7
 typedef struct set
 {
     /* data */
@@ -72,13 +72,10 @@ void SortUpto_K(float kon[][2],int k,int max)
         if(kon[min][0] > kon[j][0])
         {
           min = j;
-<<<<<<< HEAD
-        }
-=======
+
           // Swap(&(min[0]),&(kon[j][0]));
           // Swap(&(min[1]),&(kon[j][1]));
         }
->>>>>>> dc420e2dbd2fed8eb9b1a01bcb354e83928443fc
       }
       float temp = kon[min][0];
       kon[min][0] = kon[i][0];
@@ -109,9 +106,41 @@ int FindMaxClass(float kon[][2],SET *train[],int k,int clscount)
     return max+1;
 }
 
+float FindAccuracy(int k,int p,SET *test[],SET *train[]){
+
+  float distace_of_test;
+  int assign[210/r];
+  int true = 0;
+  // for(i=0;i<30;i++)
+  //   assign[i] = 0;
+  int i,j;
+  for(i=0;i<(210/r);i++){
+    float kon[210-210/r][2];
+
+    for (j=0;j<210-(210/r);j++){
+      //printf("%d\n",test[1]->class);
+
+      distace_of_test=Minkowski_distance(p,train[j]->area-test[i]->area,train[j]->perimeter-test[i]->perimeter,train[j]->compactness-test[i]->compactness,train[j]->length_of_kernel-test[i]->length_of_kernel,train[j]->width_of_kernel-test[i]->width_of_kernel,train[j]->asymmetry_coefficient-test[i]->asymmetry_coefficient,train[j]->length_of_kernel_groove-test[i]->length_of_kernel_groove);
+      kon[j][0] = distace_of_test;
+      kon[j][1] = j;
+    }
+    SortUpto_K(kon,k,210-210/r);
+
+    assign[i] = FindMaxClass(kon,train,k,3);
+    //printf("%d\t%d\n",assign[i],test[i]->class);
+    if(assign[i]==test[i]->class)
+      ++true;
+    }
+
+    return (true/(210.0/r))*100;
+}
+
+
 int main()
 {
     int l,m;
+    float final_accuracy=0;
+    float accuracy[r];
     FILE *filePointer;
     filePointer = fopen("SwapedSeeds.txt","r");
 
@@ -129,17 +158,21 @@ int main()
 
     fclose(filePointer);
     // WriteTestValues(data[5]);
-    int r=7;
+    //int r=7;
 
     // SET *test;
     // test=data[0];
     // printf("%f\n",test->area );
     SET *test[210/r];
     SET *train[210-(210/r)];
+
+    int k=11,p=4;
+    printf("\nFor k=%d , p=%d and r=%d the accuracy is :\n\n",k,p,r);
+
     for(l=0;l<7;l++){
 
       for (m=30*l;m<30*(l+1);m++){
-        test[m-(30*m)]=data[m];
+        test[m-(30*l)]=data[m];
       }
       int limit=0;
       for (m=0;m<210;m++){
@@ -148,69 +181,17 @@ int main()
           ++limit;
         }
       }
-      printf("hello %d\n",l);
           //////        finding k and p values    ////////
 
-      int k=9,p=2;
-      float distace_of_test;
-      int assign[210/r];
-      int true = 0;
-      // for(i=0;i<30;i++)
-      //   assign[i] = 0;
-      int i,j;
-      for(i=0;i<(210/r);i++){
-        float kon[210-210/r][2];
-        printf("hello%d  %d\n",i,210/r);
-
-        for (j=0;j<210-(210/r);j++){
-          printf("%d\n",j);
-
-          distace_of_test=Minkowski_distance(p,train[j]->area-test[i]->area,train[j]->perimeter-test[i]->perimeter,train[j]->compactness-test[i]->compactness,train[j]->length_of_kernel-test[i]->length_of_kernel,train[j]->width_of_kernel-test[i]->width_of_kernel,train[j]->asymmetry_coefficient-test[i]->asymmetry_coefficient,train[j]->length_of_kernel_groove-test[i]->length_of_kernel_groove);
-          kon[j][0] = distace_of_test;
-          kon[j][1] = j;
-        }
-        printf("%d\n",i);
-        SortUpto_K(kon,k,210-210/r);
-
-        assign[i] = FindMaxClass(kon,train,k,3);
-        printf("%d\t%d\n",assign[i],test[i]->class);
-        if(assign[i]==test[i]->class)
-          ++true;
-        }
-
-        printf("%d\t%f\n" ,l ,true/(210.0/r));
-
-
-    int k=9,p=2;
-    float KNN[k];
-    float distace_of_test;
-    int assign[30];
-    // for(i=0;i<30;i++)
-    //   assign[i] = 0;
-    for(i=0;i<30;i++){
-      float kon[180][2];
-      for (j=0;j<180;j++){
-        distace_of_test=Minkowski_distance(p,train[j]->area-test[i]->area,train[j]->perimeter-test[i]->perimeter,train[j]->compactness-test[i]->compactness,train[j]->length_of_kernel-test[i]->length_of_kernel,train[j]->width_of_kernel-test[i]->width_of_kernel,train[j]->asymmetry_coefficient-test[i]->asymmetry_coefficient,train[j]->length_of_kernel_groove-test[i]->length_of_kernel_groove);
-
-        // printf("%f\n",distace_of_test);
-        kon[i][0] = distace_of_test;
-        kon[i][1] = j;
-      }
-      SortUpto_K(kon,k,180);
-      int k;
-      for(k=0;k<9;k++)
-      {
-        printf("%f\t%f\n",kon[k][0],kon[k][1]);
-      }
-      final_class=mod(kon);
-      if(test[i]->class==final_class)
-        true++;
+      accuracy[l]= FindAccuracy(k,p,test,train);
+      printf("%d\t%f%\n",l+1,accuracy[l]);
 
     }
 
-
-
+    for(l=0;l<r;l++){
+      final_accuracy=final_accuracy+accuracy[l];
     }
+    printf("Final accuracy of the dataset = %f%\n",final_accuracy/(r*1.0));
 
     //////        finding k and p values    ////////
 
