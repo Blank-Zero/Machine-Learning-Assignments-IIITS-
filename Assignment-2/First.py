c72e41ac5627b4cd1c97b30694b3123b273b77f0
Hemanth <33123731+Hemanth759@users.noreply.github.com>
2019-03-02 15:08:26 +0530

    Completed first question

--- Assignment-2/First.py
+++ Assignment-2/First.py
@@ -18,21 +18,73 @@ def Shuffler(data):
     
     return newdata
 
+def LineFunc(data,slope_m,constant_c):
+    line = 0.0
+    for i in range(len(slope_m)):
+        line = line + slope_m[i] * data[i]
+    return line + constant_c
 
-dataset = []
-fileptr = open("Dataset-1.txt",'r')
-for line in fileptr:
-    data = line.split()
-    data = GetFloat(data)
-    # print(data)
-    dataset.append(data)
-fileptr.close()
+def GradientDecent(TrainingSet,slope_m,constant_c):
+    D_slope = [0.0,0.0,0.0,0.0,0.0,0.0]
+    D_const = 0.0
+    
+    for data in TrainingSet:
+        pred = LineFunc(data,slope_m,constant_c)
+        for i in range(len(D_slope)):
+            D_slope[i] = D_slope[i] + data[i] * (data[len(data)-1] - pred)
+        D_const = D_const + data[len(data)-1] - pred
 
-print(dataset[5])
+    D_const = (-2.0/len(TrainingSet))*D_const
+    for i in range(len(D_slope)):
+        D_slope[i] = (-2.0/len(TrainingSet))*D_slope[i]
 
-dataset = Shuffler(dataset)
+    return D_slope, D_const
+    
+def FindError(testset,slope,constant):
+    error = 0
+    for data in testset:
+        error = error + (data[len(data)-1] - LineFunc(data,slope,constant))**2
+    error = error/float(len(testset))
+    return error
 
-fileptr = open("testfile.txt","w")
-for i in dataset:
-    fileptr.write(str(i)+'\n')
-fileptr.close()
+
+if __name__ == "__main__":    
+    dataset = []
+    fileptr = open("Dataset-1.txt",'r')
+    for line in fileptr:
+        data = line.split()
+        data = GetFloat(data)
+        # print(data)
+        dataset.append(data)
+    fileptr.close()
+
+    # print(dataset[5])
+
+    dataset = Shuffler(dataset)
+
+    # fileptr = open("testfile.txt","w")
+    # for i in dataset:
+    #     fileptr.write(str(i)+'\n')
+    # fileptr.close()
+
+    trainset = dataset[0:200]
+    testset = dataset[200:len(dataset)]
+
+    slope_m = [0.0,0.0,0.0,0.0,0.0,0.0]
+    constant_c = 0
+    learning_step = 0.001
+    
+    for _ in range(1000):
+        D_slope, D_const = GradientDecent(trainset,slope_m,constant_c)
+        for i in range(len(slope_m)):
+            slope_m[i] = slope_m[i] - learning_step*D_slope[i]
+        constant_c = constant_c - learning_step*D_const
+    
+    error = FindError(trainset,slope_m,constant_c)
+    print("Training set error:{}".format(error))
+
+    error = FindError(testset,slope_m,constant_c)
+    print("Test set error:{}".format(error))
+
+    print("Slope:{}".format(slope_m))
+    print("constant:{}".format(constant_c))
