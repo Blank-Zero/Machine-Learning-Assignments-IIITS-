d50b01980dcadb7322806b94db9ab6dc809d95dd
Hemanth <33123731+Hemanth759@users.noreply.github.com>
2019-03-02 12:52:35 +0530

    Added shuffle func

--- Assignment-2/First.py
+++ Assignment-2/First.py
@@ -1,8 +1,23 @@
+import random
+
 def GetFloat(data):
     li = []
     for i in data:
-        li = float(i)
+        li.append(float(i))
     return li
+
+def Shuffler(data):
+    li = list(range(0,len(data),1))
+    for _ in range(1000):
+        i,j = random.randint(0,len(data)-1) , random.randint(0,len(data)-1)
+        li[i] , li[j] = li[j] , li[i]
+
+    newdata = []
+    for i in li:
+        newdata.append(data[i])
+    
+    return newdata
+
 
 dataset = []
 fileptr = open("Dataset-1.txt",'r')
@@ -13,4 +28,11 @@ for line in fileptr:
     dataset.append(data)
 fileptr.close()
 
-# print(dataset)
+print(dataset[5])
+
+dataset = Shuffler(dataset)
+
+fileptr = open("testfile.txt","w")
+for i in dataset:
+    fileptr.write(str(i)+'\n')
+fileptr.close()
